6637ff582598ecf5c3b7ae92e9fc83d8cd39d3c1
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-01-29 16:16:49 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -4,9 +4,17 @@ TABLE: users
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE
 */
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE    
+    );
 
 /*
 VALUES:
 ('Alice', 'alice@example.com'),
 ('Bob', 'bob@example.com');
 */
+INSERT INTO users VALUES
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com');
