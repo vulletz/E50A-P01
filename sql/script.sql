559d00183f6a5dbd3f38520eac91078feca9835d
vulletz <122696042+vulletz@users.noreply.github.com>
2025-01-29 18:42:12 -0600

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -9,11 +9,10 @@ CREATE TABLE USERS (
 -- insert
 INSERT INTO USERS (name, email) VALUES ('Alice', 'alice@example.com');
 INSERT INTO USERS (name, email) VALUES ('Bob', 'bob@example.com');
+INSERT INTO USERS (name, email) VALUES ('Jose', 'jose@example.com');
+INSERT INTO USERS (name, email) VALUES ('Ana', 'ana@example.com');
 
 -- fetch 
 SELECT * FROM USERS;
 
-INSERT INTO USERS (name, email) VALUES ('Jose', 'jose@example.com');
-INSERT INTO USERS (name, email) VALUES ('Ana', 'ana@example.com');
-
-SELECT * FROM USERS WHERE id = 3;
+SELECT * FROM USERS WHERE id = 2;
