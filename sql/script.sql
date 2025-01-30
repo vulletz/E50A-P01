b8cec1a8b24a0549a6dfffe2b08c922696f5374f
vulletz <122696042+vulletz@users.noreply.github.com>
2025-01-29 18:38:45 -0600

    Update script.sql 175680

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,19 @@
 
+-- create
+CREATE TABLE USERS (
+  id SERIAL UNIQUE PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  email varchar(100) NOT NULL
+);
+
+-- insert
+INSERT INTO USERS (name, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO USERS (name, email) VALUES ('Bob', 'bob@example.com');
+
+-- fetch 
+SELECT * FROM USERS;
+
+INSERT INTO USERS (name, email) VALUES ('Jose', 'jose@example.com');
+INSERT INTO USERS (name, email) VALUES ('Ana', 'ana@example.com');
+
+SELECT * FROM USERS WHERE id = 3;
